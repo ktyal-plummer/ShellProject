82b8577041b0636c11290a690df9505a1237486c
Ktyal Plummer <52009845+BasedOneven@users.noreply.github.com>
2022-03-13 20:03:19 +0000

    test 15 passes

--- wish.c
+++ wish.c
@@ -5,6 +5,21 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
+void removeSpaces(char *str)
+{
+	int i = 0;
+  int j = 0;
+  
+	while (str[i])
+	{
+		if (str[i] != ' '){
+      str[j++] = str[i];
+    }
+		i++;
+	}
+	str[j] = '\0';
+}
+
 void getCommandArguments(char* commandArgs[], char command[]){
   int i = 0;
   char *token = strtok(command, " \n \r \0");
@@ -12,6 +27,7 @@ void getCommandArguments(char* commandArgs[], char command[]){
   while (token != NULL)
   {
     commandArgs[i] = token;
+    removeSpaces(commandArgs[i]);
     token = strtok(NULL, " \n \r \0");
     i++;    
   }
@@ -235,190 +251,205 @@ void interactive()
 }
 
 void batch(int numArgs, char *textFile) {
+  int validArguments = 0;
   FILE * fp;
   char * line = NULL;
   size_t len = 0;
   ssize_t read;
+  
   char path[20][255] = { "" };
   strcpy(path[0], "bin");
+  
   char error_message[30] = "An error has occurred\n";
   char fork_error_msg[30] = "Fork failed\n";
 
+  // check for more than one input file 
   if(numArgs > 2) {
     write(STDERR_FILENO, error_message, strlen(error_message));
     exit(1);
   }
 
+  // open batch file
   fp = fopen(textFile, "r");
   if (fp == NULL) {
     write(STDERR_FILENO, error_message, strlen(error_message));
     exit(1);
   }
 
+  // main reading loop
   while ((read = getline(&line, &len, fp)) != -1) {
     char *commandArgs[10] = { NULL };
     getCommandArguments(commandArgs, line);
 
-    // parse input for output file
-    int correctInput = 1;
-    int outputFile = 0;
-    int foundOutput = 0;
-    int parseIndex = 0;
-    int fileIndex = 0;
-    char argument[255] = "";
-    char *fileArgs[3] = { NULL };
-    
-    while (commandArgs[parseIndex] != NULL) {
-      int hasOutput = 0;
-      if(strchr(commandArgs[parseIndex], '>') != NULL)
-      {
-        hasOutput = 1;
-        foundOutput = 1;
+    // check to make sure something other than whitespace was pulled 
+    for(int i = 0; i < 10; i++)
+    {
+      if(commandArgs[i] != NULL){
+        validArguments = 1;
+        break;
       }
-
-      if (hasOutput) {
-        strcpy(argument, commandArgs[parseIndex]);
-        char *token = strtok(argument, ">");
-        
-        while( token != NULL ) {
-          fileArgs[fileIndex] = token;
-          token = strtok(NULL, ">");
-          fileIndex++;
-        }
-      }
-      parseIndex++;
     }
 
-    // error if incorrect file input
-    if (foundOutput){
-      if(fileArgs[0] == NULL){
-        correctInput = 0;
-        write(STDERR_FILENO, error_message, strlen(error_message));
-      } else if(fileArgs[1] != NULL){
-        if(fileArgs[2] == NULL){
-          outputFile = 1;
-          //printf("Valid input and output file\n");
-        } else {
+    if(validArguments) {
+      // parse input for output file
+      int correctInput = 1;
+      int outputFile = 0;
+      int foundOutput = 0;
+      int parseIndex = 0;
+      int fileIndex = 0;
+      char argument[255] = "";
+      char *fileArgs[3] = { NULL };
+      
+      while (commandArgs[parseIndex] != NULL) {
+        int hasOutput = 0;
+        if(strchr(commandArgs[parseIndex], '>') != NULL)
+        {
+          hasOutput = 1;
+          foundOutput = 1;
+        }
+  
+        if (hasOutput) {
+          strcpy(argument, commandArgs[parseIndex]);
+          char *token = strtok(argument, ">");
+          
+          while( token != NULL ) {
+            fileArgs[fileIndex] = token;
+            token = strtok(NULL, ">");
+            fileIndex++;
+          }
+        }
+        parseIndex++;
+      }
+  
+      // error if incorrect file input
+      if (foundOutput){
+        if(fileArgs[0] == NULL){
           correctInput = 0;
           write(STDERR_FILENO, error_message, strlen(error_message));
+        } else if(fileArgs[1] != NULL){
+          if(fileArgs[2] == NULL){
+            outputFile = 1;
+            //printf("Valid input and output file\n");
+          } else {
+            correctInput = 0;
+            write(STDERR_FILENO, error_message, strlen(error_message));
+          }
         }
       }
-    }
-
-    // commands
-    if(correctInput){
-      // exit 
-      if(strcmp(commandArgs[0], "exit") == 0) {
-        if (commandArgs[1] == NULL) {
-          exit(0);
-        } else {
-          write(STDERR_FILENO, error_message, strlen(error_message));
-        }
-      // cd
-      } else if(strcmp(commandArgs[0], "cd") == 0) {
-        if(commandArgs[1] != NULL && commandArgs[2] == NULL) {
-          char *dir = commandArgs[1];
-          if (chdir(dir) != 0) {
+  
+      // commands
+      if(correctInput){
+        // exit 
+        if(strcmp(commandArgs[0], "exit") == 0) {
+          if (commandArgs[1] == NULL) {
+            exit(0);
+          } else {
             write(STDERR_FILENO, error_message, strlen(error_message));
-          } 
-        } else {
-          write(STDERR_FILENO, error_message, strlen(error_message));
-        }
-      // path
-      } else if(strcmp(commandArgs[0], "path") == 0) {
-        int argsIndex = 1;
-        int pathIndex = 0;
-        if (commandArgs[argsIndex] != NULL) {
-          for(int i = 0; i < 20; i++) {
-            strcpy(path[i], "");
           }
-          while (commandArgs[argsIndex] != NULL) {
-            strcpy(path[pathIndex], commandArgs[argsIndex]);
-            argsIndex++;
-            pathIndex++;
-          }
-        } else {
-          for(int i = 0; i < 20; i++) {
-            strcpy(path[i], "");
-          }
-        }
-      // other
-      } else {
-        if (strcmp(path[0], "") == 0) {
-          write(STDERR_FILENO, error_message, strlen(error_message));
-        } else {
-          if (commandArgs[0] != NULL) {
-            int foundFile = 0;
-            for(int i = 0; i < 20; i++) {
-              if(foundFile == 0 && strcmp(path[i], "") != 0){
-                int pathLen = strlen(path[i]);
-                int cmdLen = strlen(commandArgs[0]);
-                char *dir = malloc( pathLen + cmdLen + 1 );
-                int result = 1;
-  
-                strcat(dir, path[i]);
-                strcat(dir, "/");
-                strcat(dir, commandArgs[0]);
-  
-                if (access(dir, X_OK) == 0) {
-                  result = 0;
-                } else {
-                  char *startSlashes[2] = {"/", "./"};
-                  for(int i = 0; i < 1; i++) {
-                    strcpy(dir, "");
-                    strcpy(dir, startSlashes[i]);
-                    strcat(dir, path[i]);
-                    strcat(dir, "/");
-                    strcat(dir, commandArgs[0]);
-    
-                    result = access(dir, X_OK);
-                    if (!result) { break; }
-                  }
-                }
-                
-                if (result == 0) {
-                  foundFile = 1;
-                  
-                  int rc = fork();   
-                  if(rc < 0) {
-                    write(STDERR_FILENO, fork_error_msg, strlen(fork_error_msg));
-                    exit(1);
-                  } else if(rc == 0) {
-                    if(outputFile){
-                      int out = open(fileArgs[1], O_RDWR|O_CREAT|O_APPEND, 0600);
-                      if (out == -1) { 
-                        write(STDERR_FILENO, error_message, strlen(error_message)); 
-                      }
-                      int save_out = dup(fileno(stdout));
-                      if (-1 == dup2(out, fileno(stdout))) { 
-                        write(STDERR_FILENO, error_message, strlen(error_message));
-                      }
-                      
-                      strcpy(commandArgs[1], fileArgs[0]);
-                      execv(dir, commandArgs);
-
-                      fflush(stdout); 
-                      close(out);
-                      dup2(save_out, fileno(stdout));
-                      close(save_out);
-                    } else {
-                       execv(dir, commandArgs);
-                    }
-                  } else {
-                    wait(&rc);
-                  }
-                }    
-                
-                free(dir);
-              } else {
-                break;
-              }
-            }  
-            if (!foundFile) {
+        // cd
+        } else if(strcmp(commandArgs[0], "cd") == 0) {
+          if(commandArgs[1] != NULL && commandArgs[2] == NULL) {
+            char *dir = commandArgs[1];
+            if (chdir(dir) != 0) {
               write(STDERR_FILENO, error_message, strlen(error_message));
+            } 
+          } else {
+            write(STDERR_FILENO, error_message, strlen(error_message));
+          }
+        // path
+        } else if(strcmp(commandArgs[0], "path") == 0) {
+          int argsIndex = 1;
+          int pathIndex = 0;
+          if (commandArgs[argsIndex] != NULL) {
+            for(int i = 0; i < 20; i++) {
+              strcpy(path[i], "");
+            }
+            while (commandArgs[argsIndex] != NULL) {
+              strcpy(path[pathIndex], commandArgs[argsIndex]);
+              argsIndex++;
+              pathIndex++;
             }
           } else {
-            //printf("Multiargs\n");
+            for(int i = 0; i < 20; i++) {
+              strcpy(path[i], "");
+            }
+          }
+        // other
+        } else {
+          if (strcmp(path[0], "") == 0) {
+            write(STDERR_FILENO, error_message, strlen(error_message));
+          } else {
+            if (commandArgs[0] != NULL) {
+              int foundFile = 0;
+              for(int i = 0; i < 20; i++) {
+                if(foundFile == 0 && strcmp(path[i], "") != 0){
+                  int pathLen = strlen(path[i]);
+                  int cmdLen = strlen(commandArgs[0]);
+                  char *dir = malloc( pathLen + cmdLen + 1 );
+                  int result = 1;
+    
+                  strcat(dir, path[i]);
+                  strcat(dir, "/");
+                  strcat(dir, commandArgs[0]);
+    
+                  if (access(dir, X_OK) == 0) {
+                    result = 0;
+                  } else {
+                    char *startSlashes[2] = {"/", "./"};
+                    for(int i = 0; i < 1; i++) {
+                      strcpy(dir, "");
+                      strcpy(dir, startSlashes[i]);
+                      strcat(dir, path[i]);
+                      strcat(dir, "/");
+                      strcat(dir, commandArgs[0]);
+      
+                      result = access(dir, X_OK);
+                      if (!result) { break; }
+                    }
+                  }
+                  
+                  if (result == 0) {
+                    foundFile = 1;
+                    
+                    int rc = fork();   
+                    if(rc < 0) {
+                      write(STDERR_FILENO, fork_error_msg, strlen(fork_error_msg));
+                      exit(1);
+                    } else if(rc == 0) {
+                      if(outputFile){
+                        int out = open(fileArgs[1], O_RDWR|O_CREAT|O_APPEND, 0600);
+                        if (out == -1) { 
+                          write(STDERR_FILENO, error_message, strlen(error_message)); 
+                        }
+                        int save_out = dup(fileno(stdout));
+                        if (-1 == dup2(out, fileno(stdout))) { 
+                          write(STDERR_FILENO, error_message, strlen(error_message));
+                        }
+                        
+                        strcpy(commandArgs[1], fileArgs[0]);
+                        execv(dir, commandArgs);
+  
+                        fflush(stdout); 
+                        close(out);
+                        dup2(save_out, fileno(stdout));
+                        close(save_out);
+                      } else {
+                         execv(dir, commandArgs);
+                      }
+                    } else {
+                      wait(&rc);
+                    }
+                  }    
+                  
+                  free(dir);
+                } else {
+                  break;
+                }
+              }  
+              if (!foundFile) {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+              }
+            }
           }
         }
       }
